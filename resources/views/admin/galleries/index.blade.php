c09d21a017d923799c92806930a09f1c9b329eca
unknown <gauravgurung065@gmail.com>
2020-12-08 22:10:58 +0545

    changes

--- resources/views/admin/galleries/index.blade.php
+++ resources/views/admin/galleries/index.blade.php
@@ -25,21 +25,56 @@
                       </div>
                   </div>
               </div>
-              <div id="gallery-content">
-                  <div id="gallery-content-center" class="gallery-content-center-full isotope" style="position: relative; overflow: hidden; height: 4113.02px;">
+  <div class="row">
                     @foreach( $galleries as $gallery)
-                  <a href="{{$gallery->medium_image}}" data-toggle="lightbox" data-gallery="multiimages" data-title="{{$gallery->title}} >> {{$gallery->description}}">
-                    <img src="{{ $gallery->medium_image}}" class="all studio isotope-item" alt="{{$gallery->title}}" style="position: absolute; left: 0px; top: 0px; transform: translate3d(301px, 2474px, 0px);" /></a>
+                    <div class="col-sm-4" style="background-color: #f8f8f8;">
+      <small class="d-flex justify-content-end">
+          <a href="{{ route('admin.galleries.edit', $gallery->id)}}"
+          class="btn btn-sm btn-success-outline" data-original-title="Delete">
+          <i class="ti-pencil" aria-hidden="true"></i></a>
+          <form action="{{route('admin.galleries.destroy', $gallery->id) }}" method="post">
+            <input type="hidden" name="_method" value="DELETE">
+              @csrf
+              <button data-toggle="tooltip" data-original-title="Delete" 
+              type="submit" id="btnDelete" onclick="return confirm('Are you sure?')" class="btn btn-sm btn-danger-outline">
+              <i class="ti-trash" aria-hidden="true"></i></button>
+          </form> 
+      </small>
+                  <a href="{{$gallery->medium_image}}" class="image-popup-vertical-fit" data-toggle="lightbox" data-gallery="multiimages" data-title="{{$gallery->title}} >> {{$gallery->description}}">
+                    <img src="{{ $gallery->medium_image}}" class="img-responsive all studio " alt="{{$gallery->title}}"  /></a>                    
+                  </div>
                     @endforeach
                 </div>
               </div>
           </div>
+            </div>
       </div>
       <!-- /.box-body -->
     </div>
     <!-- /.box -->
 
   </section>
+
+  <div class="row">
+    <div class="col-sm-4">
+      <a class="image-popup-vertical-fit" href="../../../images/gallery/thumb/1.jpg" title="Caption. Can be aligned to any side and contain any HTML.">
+        <img src="../../../images/gallery/thumb/1.jpg" class="img-responsive" alt="">
+      </a>
+      <small>Fits horizontally and vertically</small>
+    </div>
+    <div class="col-sm-4">
+      <a class="image-popup-fit-width" href="../../../images/gallery/thumb/2.jpg" title="This image fits only horizontally.">
+        <img src="../../../images/gallery/thumb/2.jpg" class="img-responsive" alt="">
+      </a>
+      <small>Only horizontally</small>
+    </div>
+    <div class="col-sm-4">
+      <a class="image-popup-no-margins" href="../../../images/gallery/thumb/3.jpg">
+        <img src="../../../images/gallery/thumb/3.jpg" class="img-responsive" alt="">
+      </a>
+      <small>Zoom animation, close on top-right.</small>
+    </div>
+  </div>
 
 
 
